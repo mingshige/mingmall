3f049eaab76a47da51421b209ac75a674dbbc7e4
Administrator <1841193218@qq.com>
2021-11-28 08:25:58 +0800

    默认的

--- mingmall-coupon/src/main/java/com/atming/mingmall/coupon/MingmallCouponApplication.java
+++ mingmall-coupon/src/main/java/com/atming/mingmall/coupon/MingmallCouponApplication.java
@@ -3,10 +3,13 @@ package com.atming.mingmall.coupon;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class MingmallCouponApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(MingmallCouponApplication.class, args);
     }
 
---
+++ mingmall-product/src/test/java/com/atming/mingmall/product/MingmallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atming.mingmall.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MingmallProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
